ad84115a87871dc700b1b128dc0db5513cdd686a
ephiguxta <fabiogustavo@tuta.io>
2024-10-19 13:06:09 -0400

    tratando os dados em formato json

--- website/show_data.js
+++ website/show_data.js
@@ -3,6 +3,8 @@ window.addEventListener("DOMContentLoaded", () => {
 	const websocket = new WebSocket("ws://localhost:12345/");
 
 	websocket.onmessage = ({ data }) => {
-		document.getElementById("random").innerHTML = data;
+		const json = JSON.parse(data)
+		document.getElementById("velo").innerHTML = json["velo"];
+		document.getElementById("temp").innerHTML = json["temp"];
 	};
 });
--- website/websockets_srv.py
+++ website/websockets_srv.py
@@ -1,16 +1,21 @@
 import serial
 import asyncio
 from websockets.asyncio.server import serve
+from sys import exit
 
-ser = serial.Serial('/dev/ttyUSB0')
+port = "/dev/ttyUSB0"
+try:
+    ser = serial.Serial(port)
+except:
+    print(f"{port} inacessível")
+    exit(1)
 
 async def get_string(websocket):
     while True:
-        data = str(ser.readline())
+        data = ser.readline()
         data = data.decode()
-        data = data[:2]
+        data = data[:-2]
         await websocket.send(data)
-
 
 async def main():
     async with serve(get_string, "localhost", 12345):
