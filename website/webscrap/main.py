c755e4eaf50bc77ac66481c76f889206beddfce4
ephiguxta <fabiogustavo@tuta.io>
2025-03-29 16:23:01 -0400

    esboço para pegar a posição da equipe no quadro geral do torneio nacional

---
+++ website/webscrap/main.py
@@ -0,0 +1,26 @@
+import requests
+import re
+from bs4 import BeautifulSoup
+
+URL="https://resultados.bajasaebrasil.net/prova.php?id=25BR_GER"
+
+req = requests.get(URL)
+
+if req.status_code == 200:
+    soup = BeautifulSoup(req.text, 'html.parser')
+
+    table = soup.find(id="myTable")
+
+    data = []
+    for line in table.tbody.children:
+        if re.search(r'Baja Guaicurus', line.text):
+
+            i = 0
+            for field in line.children:
+                if i == 0 or i == 10:
+                    data.append(field.text)
+
+                i += 1
+
+    print(f"Posição: {data[0]}")
+    print(f"Pontuação: {data[1]}")
